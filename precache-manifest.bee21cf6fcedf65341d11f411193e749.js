8560144d9707a0d2ce4723d829e40825b320f646
KosBelozyorov <KosBelozyorov@gmail.com>
2019-09-18 23:54:52 +0300

    Updates

---
+++ precache-manifest.bee21cf6fcedf65341d11f411193e749.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbf0e676d534114ce27cac8621e3384",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "87820a49295d7b26c1d7",
+    "url": "/react_add-todo-form/static/css/main.fa824e6d.chunk.css"
+  },
+  {
+    "revision": "38805627cc5ae034a067",
+    "url": "/react_add-todo-form/static/js/2.01469995.chunk.js"
+  },
+  {
+    "revision": "87820a49295d7b26c1d7",
+    "url": "/react_add-todo-form/static/js/main.041f5982.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
